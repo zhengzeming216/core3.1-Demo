ff98b080e58cda5f73f20a85c025bbe528b84591
zhengzeming <923536859@qq.com>
2020-11-07 21:41:25 +0800

    123

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace core3._1_Demo
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
